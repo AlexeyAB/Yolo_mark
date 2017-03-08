746e590b106080dbb3d205dd453b2fcea5e880a3
AlexeyAB84 <alexeyab84@gmail.com>
2017-03-08 18:08:08 +0300

    bash-fixed

--- linux_mark.sh
+++ linux_mark.sh
@@ -1,7 +1,7 @@
 echo     Example how to start marking bouded boxes for training set Yolo v2
 
 
-./yolo_mark data/img data/train.txt data/obj.names
+./yolo_mark x64/Release/data/img x64/Release/data/train.txt x64/Release/data/obj.names
 
 
 pause

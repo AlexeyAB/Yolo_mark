1bc17e421d43335958d73676874cbeeeb2910e1d
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-08-21 14:50:41 +0200

    build release only to halve CI time

--- build.sh
+++ build.sh
@@ -45,3 +45,7 @@ cmake .. -DCMAKE_BUILD_TYPE=Release ${vcpkg_define} ${vcpkg_triplet_define} ${ad
 cmake --build . --target install -- -j${number_of_build_workers}
 #cmake --build . --target install --parallel ${number_of_build_workers}  #valid only for CMake 3.12+
 cd ..
+
+# In case you want to discard changes from examples, it's useful to run this command:
+# git update-index --assume-unchanged data/img/air1.jpg data/img/air1.txt data/img/air2.jpg data/img/air2.txt data/img/air3.jpg data/img/air3.txt data/img/air4.jpg data/img/air4.txt data/img/air5.jpg data/img/air5.txt data/img/air6.jpg data/img/air6.txt data/img/bird1.jpg data/img/bird1.txt data/img/bird2.jpg data/img/bird2.txt data/img/bird3.jpg data/img/bird3.txt data/img/bird4.jpg data/img/bird4.txt data/obj.data data/obj.names data/train.txt
+

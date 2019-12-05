e88601580c03897c996ffa6c77028a5cca861bee
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-12-05 07:23:16 -0600

    changed shutil.copy to shutil.move for test set creation since test set should not be in training set (they should be seperate)

--- x64/Release/test_set_generator.py
+++ x64/Release/test_set_generator.py
@@ -13,7 +13,9 @@ for i in range(0,len(files)):
 no_of_images = len(files)
 print(no_of_images)
 
-shuffle = np.random.permutation(no_of_images) # creates random permutation of 602 images
+#shuffle = np.random.permutation(no_of_images) # creates random permutation of 602 images
+shuffle = np.random.permutation(no_of_images) # creates random permutation of 676
+
 print(shuffle)
 
 list = [1,3,5,9,7, 9,10]
@@ -25,9 +27,11 @@ os.mkdir(os.path.join('data', 'test')) # creates empty VALID folder
 #for files in glob.glob('data/images/*.jpg'):
     #print (files)
 
-for i in shuffle[:240]:
+# %15 of 676
+# need to move/seperate the valid images out of the imgs folder
+for i in shuffle[:101]:
     print(files[i])
-    shutil.copy(files[i],'data/test')
+    shutil.move(files[i],'data/test')
     folder = files[i].split('/')[-1].split('.')[0]
     print(folder)
     image = files[i].split('/')[-1]
